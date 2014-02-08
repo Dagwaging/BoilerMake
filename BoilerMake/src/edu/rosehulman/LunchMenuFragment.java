1b3be9dfa4add6f47e520555cfa2538040efbf3d
chinchalupa <chinchalupa@gmail.com>
2014-02-08 04:16:50 -0500

    puppies

---
+++ BoilerMake/src/edu/rosehulman/LunchMenuFragment.java
@@ -0,0 +1,47 @@
+package edu.rosehulman;
+
+import android.app.Fragment;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class LunchMenuFragment extends Fragment {
+	private LinearLayout breakfast;
+	private TextView breakfast_text;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		ScrollView view = (ScrollView) inflater.inflate(R.layout.lunchmenu, container);
+		
+		breakfast = (LinearLayout) view.findViewById(R.id.menu_breakfast);
+		breakfast_text = (TextView) breakfast.findViewById(R.id.textView1);
+		
+		return view;
+	}
+	
+	@Override
+	public void onStart() {
+		MenuLoaderTask task = new MenuLoaderTask();
+		task.execute();
+	}
+
+	private class MenuLoaderTask extends AsyncTask<Void, Void, String> {
+
+		@Override
+		protected String doInBackground(Void... params) {
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			breakfast_text.setText(result);
+		}
+		
+	}
+}

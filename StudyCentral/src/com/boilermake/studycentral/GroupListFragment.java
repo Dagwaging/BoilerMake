607ab90d967947b129f95f06dffbd9ec2e8f9888
Dagwaging <dagwaging@gmail.com>
2014-02-08 06:46:22 -0500

    Added barebones fragment and activity classes

---
+++ StudyCentral/src/com/boilermake/studycentral/GroupListFragment.java
@@ -0,0 +1,7 @@
+package com.boilermake.studycentral;
+
+import android.support.v4.app.Fragment;
+
+public class GroupListFragment extends Fragment {
+
+}
---
+++ StudyCentral/src/com/boilermake/studycentral/JoinGroupActivity.java
@@ -0,0 +1,7 @@
+package com.boilermake.studycentral;
+
+import android.app.Activity;
+
+public class JoinGroupActivity extends Activity {
+
+}
